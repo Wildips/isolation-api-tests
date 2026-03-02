53bcb001e61022b5ccaf75c932b75418e91a4a3d
Wildips <zak125@mail.ru>
2026-03-03 01:09:29 +0300

    Конфигурация тестового окружения. Шаг 10.

--- tests/config.py
+++ tests/config.py
@@ -4,21 +4,20 @@ from tests.tools.config.grpc import GRPCClientTestConfig
 from tests.tools.config.http import HTTPClientTestConfig
 from tests.tools.config.kafka import KafkaClientTestConfig
 
+from tests.tools.config.grpc import GRPCServerTestConfig
+from tests.tools.config.http import HTTPServerTestConfig
+
 
 class TestSettings(BaseSettings):
-    """
-    Корневая конфигурация тестового окружения.
-
-    Является единой точкой входа для всех настроек,
-    используемых в тестах и клиентах.
-    """
-
     model_config = SettingsConfigDict(
         extra="allow",
         env_file="./tests/.env",
         env_file_encoding="utf-8",
         env_nested_delimiter=".",
     )
+
+    mock_http_server: HTTPServerTestConfig
+    mock_grpc_server: GRPCServerTestConfig
 
     gateway_http_client: HTTPClientTestConfig
     gateway_grpc_client: GRPCClientTestConfig
