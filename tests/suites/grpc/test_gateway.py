4d013fa9e28583ef8a2851e6d89b836382bf47fd
Wildips <zak125@mail.ru>
2026-04-01 13:23:37 +0300

    Сценарные тесты gRPC API. Шаг 7.

--- tests/suites/grpc/test_gateway.py
+++ tests/suites/grpc/test_gateway.py
@@ -3,6 +3,7 @@ import pytest
 
 from tests.assertions.grpc.gateway import (
     assert_get_user_details_response_user_with_active_credit_card_account,
+    assert_get_account_details_response_user_with_active_debit_card_account,
 )
 from tests.clients.grpc.gateway.client import GatewayGRPCTestClient
 from tests.context.base import RequestContext
@@ -26,8 +27,8 @@ class TestGatewayGRPC:
     @allure.story(AllureStory.GET_USER_DETAILS)
     @allure.title("[gRPC] Get user details. User with active credit card account")
     def test_get_user_details_user_with_active_credit_card_account(
-        self,
-        gateway_grpc_test_client: GatewayGRPCTestClient,
+            self,
+            gateway_grpc_test_client: GatewayGRPCTestClient,
     ):
         response = gateway_grpc_test_client.get_user_details(
             RequestContext(
@@ -36,5 +37,21 @@ class TestGatewayGRPC:
         )
 
         assert_get_user_details_response_user_with_active_credit_card_account(
+            response
+        )
+
+    @allure.story(AllureStory.GET_ACCOUNT_DETAILS)
+    @allure.title("[gRPC] Get account details. User with active debit card account")
+    def test_get_account_details_user_with_active_debit_card_account(
+            self,
+            gateway_grpc_test_client: GatewayGRPCTestClient,
+    ):
+        response = gateway_grpc_test_client.get_account_details(
+            RequestContext(
+                scenario=Scenario.USER_WITH_ACTIVE_DEBIT_CARD_ACCOUNT
+            )
+        )
+
+        assert_get_account_details_response_user_with_active_debit_card_account(
             response
         )
